3126e00c86c543d8a43e09d021858b6a51432ae2
Darshan Yadav <121230952+drshny03@users.noreply.github.com>
2025-02-23 23:21:06 +0530

    Delete src/main/java/com/bridgelab/employeepayrollapp/repository directory

--- src/main/java/com/bridgelab/employeepayrollapp/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelab.employeepayrollapp.repository;
-
-import com.bridgelab.employeepayrollapp.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
