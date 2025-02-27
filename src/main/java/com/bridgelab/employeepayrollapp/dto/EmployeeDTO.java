449b0604c1b451ac73c635d775b4570243577d5c
drshny03 <darshanyadav7049@gmail.com>
2025-02-27 16:15:49 +0530

    [Darshan] Add . ensure validations is done on the payroll DTO

--- src/main/java/com/bridgelab/employeepayrollapp/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelab/employeepayrollapp/dto/EmployeeDTO.java
@@ -3,6 +3,11 @@ package com.bridgelab.employeepayrollapp.dto;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.time.LocalDate;
 import java.util.List;
 
 @Setter
@@ -12,14 +17,24 @@ import java.util.List;
 
 public class EmployeeDTO {
     //attribute
+    @NotNull
     private int id;
     //add validation annotation
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z]{2,}$", message = "employee name is not valid")
     private String name;
+    @Min(value = 15000, message = "min salary should be more than 15000")
     private long salary;
     //adding more properties
+    @Pattern(regexp = "female|male", message = "gender needs to be female or male")
     private String gender;
-    private String startDate;
+    @JsonFormat(pattern = "dd MMM yyyy")
+    @NotNull(message = "start date should not be null")
+    private LocalDate startDate;
+    @NotBlank(message = "profile pic cannot be empty")
     private String profilePic;
-    private List<String> department;
+    @NotNull(message = "department should not be null")
+    @Size(min = 1, message = "employee should have at least one department")
+    private List<String> departments;
+    @NotBlank(message = "note should not be empty")
+    private String note;
 }
--- src/main/java/com/bridgelab/employeepayrollapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bridgelab/employeepayrollapp/exception/GlobalExceptionHandler.java
@@ -1,22 +1,37 @@
 package com.bridgelab.employeepayrollapp.exception;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception){
+        log.error("invalid date format", exception.getMessage());
+        return new ResponseEntity<>("Invalid date format", HttpStatus.BAD_REQUEST);
+    }
     //method to handle name field validation exception
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleException(MethodArgumentNotValidException exception){
-        FieldError fieldError = exception.getFieldError();
-        assert fieldError != null;
-        return new ResponseEntity<>(fieldError.getDefaultMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Map<String, String>> handleException(MethodArgumentNotValidException exception){
+        //map to store error and message
+        Map<String, String> errorMap = new HashMap<>();
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        for (FieldError fieldError : fieldErrors){
+            errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
     }
     //method to handle employee not found exception
     @ExceptionHandler(EmployeeNotFoundException.class)
